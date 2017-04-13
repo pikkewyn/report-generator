fbac79392b16b416f1324921cc7c5a5770347f83
Jan Cerkaski <cerkaski.jan@gmail.com>
2017-04-13 13:06:40 +0200

    1) functions 2) backup

--- reportgen.sh
+++ reportgen.sh
@@ -4,7 +4,7 @@ day="$(date '+%d-%m')"
 dat="date '+%H:%M %d-%m: '"
 DIRECTORY="/home/$USER/reports"
 FILE="/home/$USER/report-$day.dat"
-
+BIN_PATH="$HOME/.local/bin"
 monitor()
 {
   sleep "$((15 - $(date '+%-M') % 15))m"
@@ -12,22 +12,46 @@ monitor()
   while true
   do
     echo -n "$(eval $dat)" >> $FILE
-    info=$(zenity --text-info \
+    cp $FILE ${FILE}.bak
+    local info=$(zenity --text-info \
           --title="Report" \
           --editable \
           --filename=$FILE 2> /dev/null)
     if [[ $? -eq 0 ]]; then
+        local new_lines=$(echo $info | awk 'END{print NR}')
+        local old_lines=$(awk 'END{print NR}')
+
+        if [[ $new_lines -gt $old_lines ]]; then
+            rm ${FILE}.bak
+        fi
+
         echo "$info" >| $FILE
         if [[ "$info" =~ "KONIEC" ]]; then
-          break;
+            break;
         fi
     fi
     sleep "$((15 - $(date '+%-M') % 15))m"
   done
 }
 
+install()
+{
+    if [[ ! -f $BIN_PATH ]]; then
+        echo "Missing $BIN_PATH install path"
+    else
+        ln -vfs $0 "${BIN_PATH}/reportgen"
+    fi
+}
+
+stop()
+{
+    kill -SIGSTOP $(pidof -x $0)
+}
+
+cont()
+{
+    kill -SIGCONT $(pidof -x $0)
+}
+
 mkdir -p $DIRECTORY
 monitor &
-
-#TODO:
-#zenity auto scroll patch ( or maybe it already is in source )
